b91b3650fb0d030f7087d553fafc941a08207ab6
wangcheng <1246550843@qq.com>
2020-07-29 18:30:21 +0800

    Spring 源码环境初始化

---
+++ spring-analyse/src/main/java/beans/Chicken.java
@@ -0,0 +1,14 @@
+package beans;
+
+public class Chicken {
+
+	private Egg egg;
+
+	public Egg getEgg() {
+		return egg;
+	}
+
+	public void setEgg(Egg egg) {
+		this.egg = egg;
+	}
+}
---
+++ spring-analyse/src/main/java/beans/Egg.java
@@ -0,0 +1,14 @@
+package beans;
+
+public class Egg {
+
+	private Chicken chicken;
+
+	public Chicken getChicken() {
+		return chicken;
+	}
+
+	public void setChicken(Chicken chicken) {
+		this.chicken = chicken;
+	}
+}
